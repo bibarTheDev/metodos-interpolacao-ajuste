293f76fcd43ecb80ab09022a85259cafd1505bde
luccabibar <luccabibar@gmail.com>
2023-07-02 21:43:56 -0300

    correcao no coeficiente determinacao para suportar tabela de pontos

--- 4-coef-determ.c
+++ 4-coef-determ.c
@@ -5,15 +5,15 @@
  * <arg-2>: tabela, pontos tabelados (E) 
  * <arg-3>: vetorY, valores Y ajustado (E)
 */
-double CoefDeterminacao(int npts, double pts[MTXSIZE], double y[MTXSIZE])
+double CoefDeterminacao(int npts, double pts[2][MTXSIZE], double y[MTXSIZE])
 {
     double r;
     double sumY = 0, sumYQ = 0, sumErrQ = 0;
 
     for(int i = 0; i < npts; i++){
-        sumY += pts[i];
-        sumYQ += pow(pts[i], 2);
-        sumErrQ += pow(pts[i] - y[i], 2);
+        sumY += pts[1][i];
+        sumYQ += pow(pts[1][i], 2);
+        sumErrQ += pow(pts[1][i] - y[i], 2);
     }
     
     printf("%lf | %lf | %lf\n", sumY, sumYQ, sumErrQ);
